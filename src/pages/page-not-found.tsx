53f68ebf1809047056b5e303466366c3abc101cf
molamike <120581758+molamikedevs@users.noreply.github.com>
2025-10-29 05:53:24 +0400

    update page-not-found css

--- src/pages/page-not-found.tsx
+++ src/pages/page-not-found.tsx
@@ -1,7 +1,27 @@
-export default function PageNotFound() {
-  return (
-    <div>
-      <h1>Page not found 😢</h1>
-    </div>
-  );
+import { useLocation, Link } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const NotFound = () => {
+	const location = useLocation()
+
+	useEffect(() => {
+		console.error(
+			'404 Error: User attempted to access non-existent route:',
+			location.pathname
+		)
+	}, [location.pathname])
+
+	return (
+		<div className="not-found">
+			<div>
+				<h1>404</h1>
+				<p>Oops! Page not found</p>
+				<Link to="/" className="not-found-link">
+					Return to Home
+				</Link>
+			</div>
+		</div>
+	)
 }
+
+export default NotFound
